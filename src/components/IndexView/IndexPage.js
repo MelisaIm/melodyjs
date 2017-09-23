bf17550c623204a2a81e1522d12f36d758904fc4
Melisa Im <melisaevelynim@gmail.com>
2017-09-23 13:51:20 -0700

    Added meta tags to generate preview on social media sites

--- src/components/IndexView/IndexPage.js
+++ src/components/IndexView/IndexPage.js
@@ -14,7 +14,9 @@ export default function IndexPage() {
 							<span className="jumbotron-text">Welcome to MelodyJS!</span>
 						</h1>
 						<p>
-							<span className="jumbotron-text">MelodyJS is a visual music canvas</span>
+							<span className="jumbotron-text">
+								MelodyJS is a visual music canvas, compatible with Chrome and Safari
+							</span>
 						</p>
 						<p>
 							<LinkContainer to="/studio" exact>
