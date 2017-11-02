93887dcfddf83b3eb48bcc1e3f07624accf04db8
Melisa Im <melisaevelynim@gmail.com>
2017-11-02 10:05:19 -0700

    Auth files

---
+++ src/helperFunctions/checkAuthentication.js
@@ -0,0 +1,27 @@
+import getUser from '../requests/users/getUser';
+import jwtDecode from 'jwt-decode';
+
+export default async function checkAuthentication({ baseUrl }) {
+	try {
+		const token = localStorage.getItem('token');
+
+		if (!token) {
+			localStorage.removeItem('token');
+			return null;
+		}
+
+		const { sub: userId, exp: expiration } = jwtDecode(token);
+
+		if (expiration * 1000 > Date.now()) {
+			localStorage.removeItem('token');
+			return null;
+		}
+
+		const user = await getUser(userId, { baseUrl, token });
+
+		return Object.assign({}, token, user);
+	} catch (error) {
+		localStorage.removeItem('token');
+		return null;
+	}
+}
