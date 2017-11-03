a21cef322ae000eca60a15a8b0899ec52d863440
Melisa Im <melisaevelynim@gmail.com>
2017-11-03 06:59:02 -0700

    Refactor deleteSongProcess

--- src/redux/thunks/deleteSongProcess.js
+++ src/redux/thunks/deleteSongProcess.js
@@ -2,9 +2,11 @@ import deleteSong from '../../requests/deleteSong';
 
 export default function deleteSongProcess(id, history) {
 	return (dispatch, getState, env) => {
+		const { loggedUser, token } = getState();
 		return deleteSong(id, {
 			baseUrl: env.BASE_URL,
-			token: env.AIRTABLE_TOKEN
+			token: token,
+			authorId: loggedUser.id
 		}).then(result => {
 			dispatch({ type: 'DELETE_SONG', id });
 			history.push(`/library`);
--- src/requests/deleteSong.js
+++ src/requests/deleteSong.js
@@ -1,8 +1,9 @@
-export default function deleteSong(id, { baseUrl, token }) {
+export default function deleteSong(id, { baseUrl, token, authorId }) {
 	return fetch(`${baseUrl}/songs/${id}`, {
 		method: 'DELETE',
 		headers: {
-			// Authorization: `Bearer ${token}`
+			Authorization: `Bearer ${token}`,
+			'Content-Type': 'application/json'
 		}
 	})
 		.then(response => response.json())
