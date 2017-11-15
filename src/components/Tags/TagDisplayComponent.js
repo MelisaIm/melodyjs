0026444b36d7b5eb81e54edcc3ef434a3b85b2f7
Melisa Im <melisaevelynim@gmail.com>
2017-11-14 18:45:13 -0800

    Do not display tags if there are none

--- src/components/Tags/TagDisplayComponent.js
+++ src/components/Tags/TagDisplayComponent.js
@@ -1,19 +1,18 @@
 import React from 'react';
 
-export default function TagDisplayComponent({ tags = ['happy', 'weird', 'new'], song }) {
+export default function TagDisplayComponent({ tags, song }) {
 	const colors = ['red', 'black', 'blue', 'green', 'pink', 'purple', 'violet', 'indigo'];
+	return tags
+		? <div className="tagsContainer">
+				{tags.map((tag, index) => {
+					let color = colors[Math.floor(Math.random() * 7)];
 
-	return (
-		<div className="tagsContainer">
-			{tags.map((tag, index) => {
-				let color = colors[Math.floor(Math.random() * 7)];
-
-				return (
-					<div className={`tag ${color}`} key={`${index}${tag}`}>
-						{tag}
-					</div>
-				);
-			})}
-		</div>
-	);
+					return (
+						<div className={`tag ${color}`} key={`${index}${tag}`}>
+							{tag}
+						</div>
+					);
+				})}
+			</div>
+		: null;
 }
